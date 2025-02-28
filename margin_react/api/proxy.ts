5f11981981da498cc3f769de8b46d766ad87d3d2
salamonszilard <salamonszilard@gmail.com>
2025-02-28 18:04:09 +0100

    feature: tsconfig.json

--- margin_react/api/proxy.ts
+++ margin_react/api/proxy.ts
@@ -3,21 +3,19 @@ import axios, { AxiosError } from "axios";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    console.log("Proxy request received:", req.url); // Hibakereséshez
-    const response = await axios.get(
-      `https://palace-poker.hu/ujadmin/wp-json/wp/v2/posts${
-        req.url?.replace("/api/proxy", "") || ""
-      }`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
+    console.log("Proxy request received:", req.url);
+    const apiUrl = `https://palace-poker.hu/ujadmin/wp-json/wp/v2/posts${
+      req.url?.replace("/api/proxy", "") || ""
+    }`;
+    const response = await axios.get(apiUrl, {
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
+    });
     res.setHeader(
       "Access-Control-Allow-Origin",
       "https://poker-szombathely.vercel.app",
-    ); // Pontos origin
+    );
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     if (response.headers["x-wp-total"]) {
       res.setHeader("X-WP-Total", response.headers["x-wp-total"]);
@@ -25,9 +23,18 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.status(response.status).json(response.data);
   } catch (error) {
     const axiosError = error as AxiosError<{ message?: string }>;
-    console.error("Proxy error:", axiosError.message);
+    console.error(
+      "Proxy error:",
+      axiosError.message,
+      axiosError.response?.status,
+    );
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://poker-szombathely.vercel.app",
+    );
     res.status(axiosError.response?.status || 500).json({
       message: axiosError.message || "Unknown error occurred",
+      status: axiosError.response?.status,
     });
   }
 }
